628112a85d216e35a98088a55e34c7bea08b633d
Benjamin C C Hume <didillysquat@gmail.com>
2020-01-15 16:24:25 +0300

    improve argv indexing for compare_bams.py

--- bin/compare_bams.py
+++ bin/compare_bams.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """Out put how many sequences are found in common between 
 two sam files that only contain proper match sequences.
 The iput to this script should be two files on the command line.
@@ -11,10 +12,10 @@ import sys
 class CompareSAMS:
     def __init__(self):
         # get list of seq names in file one
-        with open(sys.argv[1], 'r') as f:
+        with open(sys.argv[-2], 'r') as f:
             self.seq_names_set_one = set([line.split()[0] for line in f])
         # get list of seq names in file two
-        with open(sys.argv[2], 'r') as f:
+        with open(sys.argv[-1], 'r') as f:
             self.seq_names_set_two = set([line.split()[0] for line in f])
         self.len_one = len(self.seq_names_set_one)
         self.len_two = len(self.seq_names_set_two)
@@ -24,10 +25,10 @@ class CompareSAMS:
         self.total_unique_seqs = len(self.seq_names_set_one.union(self.seq_names_set_two))
         # print stats
         print(
-            f'{self.len_one} proper pairs in {sys.argv[1]}\n'
-            f'{self.len_two} proper pairs in {sys.argv[2]}\n'
-            f'{self.len_one-self.seqs_in_common} unique proper pairs in {sys.argv[1]}\n'
-            f'{self.len_two-self.seqs_in_common} unique proper pairs in {sys.argv[2]}\n'
+            f'{self.len_one} proper pairs in {sys.argv[-2]}\n'
+            f'{self.len_two} proper pairs in {sys.argv[-1]}\n'
+            f'{self.len_one-self.seqs_in_common} unique proper pairs in {sys.argv[-2]}\n'
+            f'{self.len_two-self.seqs_in_common} unique proper pairs in {sys.argv[-1]}\n'
             f'{self.seqs_in_common} proper pairs found in common ({self.seqs_in_common/self.total_unique_seqs})\n'
             )
 cs = CompareSAMS()
